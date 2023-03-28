d0c53d5d0dda8779987d0fee0b41f3315e53c315
tawrat41 <tawratur.rashid@gmail.com>
2023-03-29 02:52:28 +0600

    done
    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
+
+// function Person(props){
+//   console.log(props)
+//   return (
+//     <div className='container'>
+//       <h1>{props.Name}</h1>
+//       <p>Profession: Developer</p>
+//     </div>
+//   )
+// }
+
+// function Person2(){
+//   return (
+//     <div className='container2'>
+//       <h1>Tawratur Rashid</h1>
+//       <p>Profession: Developer</p>
+//     </div>
+//   )
+// }
+
+// function App() {
+//   return (
+    
+//     <div className="App">
+//       <Counter></Counter>
+//     </div>
+//   );
+// }
+
+function ExternalUsers(){
+  const [users, setUsers] = useState([]);
+
+  useEffect( ()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  } ,[]); 
+
+  return(
+    <div>
+      <h1>Externl Users</h1>
+      <p>{users.length}</p>
+      {
+        users.map(user => <li>{user.name}</li>)
+      }
+    </div>
+  )
+}
+
+// function Counter(){
+//   const [count, setCount] = useState(33);
+//   // console.log(count);
+//   const increaseCount = () => setCount(count+1);
+//   const decreaseCount = () => setCount(count-1);
+//   return(
+//     <div>
+//       <h1>Count: {count}</h1>
+//       <button onClick={increaseCount}>Increase</button>
+//       <button onClick={decreaseCount}>Decrease</button>
+//     </div>
+//   )
+// }
 
 export default App;
